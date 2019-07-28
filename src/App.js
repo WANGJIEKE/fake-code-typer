b44f095915cfeea0acf8b84cf746cc7779147c5e
Tongjie <tjwang0221@gmail.com>
2019-07-27 19:02:32 -0700

    it works

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
+import hljs from 'highlight.js';
 import './App.css';
+import 'highlight.js/styles/atom-one-dark.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getRandIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nextCharIndex: 0
+    };
+  }
+
+  showNextChar(isBackSpace) {
+    if (isBackSpace) {
+      if (this.state.nextCharIndex <= 0) {
+        this.setState({nextCharIndex: 0});
+        return;
+      }
+
+      this.setState((state) => {
+        return {
+          nextCharIndex: state.nextCharIndex - 1
+        }
+      });
+
+    } else {
+      if (this.state.nextCharIndex >= this.props.content.length) {
+        document.removeEventListener('keydown', this.showNextChar);
+        return;
+      }
+  
+      this.setState((state) => {
+        return {
+          nextCharIndex: state.nextCharIndex + getRandIntInclusive(1, 5)
+        }
+      });
+    }
+  }
+
+  componentDidMount() {
+    hljs.initHighlighting();
+    document.addEventListener('keydown', (event) =>{
+      switch (event.key) {
+        case 'Escape':
+        case 'F11':
+        case 'F12':
+          break;
+        case 'Backspace':
+          this.showNextChar(true);
+          window.scrollTo(0, document.body.clientHeight);
+          event.preventDefault();
+          break;
+        default:
+          this.showNextChar();
+          window.scrollTo(0, document.body.clientHeight);
+          event.preventDefault();
+      }
+    });
+  }
+
+  componentDidUpdate() {
+    document.querySelectorAll('pre code').forEach((block) => {
+      hljs.highlightBlock(block);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <pre>
+          <code className="python">{this.props.content.slice(0, this.state.nextCharIndex)}</code>
+        </pre>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,112 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const CODE = `#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""module for removing protection in .xlsx and .xlsm files"""
+
+import argparse
+import os
+import pathlib
+import re
+import shutil
+import sys
+import zipfile
+
+TMP_DIR_PATH = '.xlsx_crack_tmp'
+WORK_BOOK_PATH = 'xl/workbook.xml'
+WORK_SHEETS_DIR_PATH = 'xl/worksheets'
+
+
+def _make_copy(path: str) -> str:
+    """make a copy of original Excel workbook"""
+    return shutil.copy2(path, path.replace('.xls', '.cracked.xls'))  # make it also work with .xlsm file
+
+
+def _print_error_msg(e: Exception) -> None:
+    """basically printing error message without the huge traceback"""
+    print(f'{sys.argv[0]}: error: type: {str(type(e))[8:-2]}; message: {str(e)}',
+          file=sys.stderr)
+    exit(1)
+
+
+def _parse_args() -> str:
+    """parsing commandline arguments"""
+    parser = argparse.ArgumentParser(description='Remove .xlsx file password')
+    parser.add_argument(dest='file_name', help='path to source file', type=str)
+    args = parser.parse_args()
+    return args.file_name
+
+
+def remove_password(path: str) -> None:
+    """
+    Remove the password of a given Microsoft Excel workbook (.xlsx and .xlsm)
+
+    @param path: a path to the file
+    @return None (but a no password version of the original file will be created)
+    """
+    if not (path.endswith('.xlsx') or path.endswith('.xlsm')):
+        print(f'{sys.argv[0]}: error: incorrect file type; only support .xlsx and .xlsm file for now', file=sys.stderr)
+        exit(1)
+    try:
+        # extract files to a directory
+        copy_path = pathlib.Path(_make_copy(path))
+        temp_dir = copy_path.parent / pathlib.Path(TMP_DIR_PATH)
+        temp_dir.mkdir(exist_ok=True)
+        file_copy = zipfile.ZipFile(copy_path)
+        file_copy.extractall(temp_dir)
+        file_copy.close()
+        os.remove(str(copy_path))
+
+        # remove password inside workbook
+        with open(f'{temp_dir}/{WORK_BOOK_PATH}', 'r') as workbook:
+            data = workbook.read()
+            data = "".join(re.split(r'<workbookProtection[^>]*/>', data))
+
+        with open(f'{temp_dir}/{WORK_BOOK_PATH}', 'w') as workbook:
+            workbook.write(data)
+
+        # remove password inside worksheet(s)
+        worksheet_root = pathlib.Path(f'{temp_dir}/{WORK_SHEETS_DIR_PATH}')
+        for item in worksheet_root.iterdir():
+            # POSIX and Windows use different separator
+            result = re.match(r'^sheet(0|[1-9][0-9]*)\.xml$', str(item).rsplit(os.sep, 1)[1])
+            if item.is_file() and result is not None:
+                with open(str(item), 'r') as worksheet:
+                    data = worksheet.read()
+                    data = "".join(re.split(r'<sheetProtection[^>]*/>', data))
+
+                with open(str(item), 'w') as worksheet:
+                    worksheet.write(data)
+
+        # pack files into a zip file
+        shutil.make_archive(str(copy_path), 'zip', temp_dir)
+        shutil.rmtree(temp_dir)
+        shutil.move(str(f'{copy_path}.zip'), copy_path)
+
+        print(f'{sys.argv[0]}: notice: cracked file is {copy_path}')
+
+    except zipfile.BadZipFile as e:
+        _print_error_msg(e)
+    except zipfile.LargeZipFile as e:
+        _print_error_msg(e)
+    except KeyError as e:
+        _print_error_msg(e)
+    except ValueError as e:
+        _print_error_msg(e)
+    except OSError as e:
+        _print_error_msg(e)
+    except Exception:
+        print(f'{sys.argv[0]}: error: unexpected error, see Traceback below and probably open an issue\n', file=sys.stderr)
+        raise
+
+
+if __name__ == '__main__':
+    remove_password(_parse_args())
+`;
+
+ReactDOM.render(<App content={CODE} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
