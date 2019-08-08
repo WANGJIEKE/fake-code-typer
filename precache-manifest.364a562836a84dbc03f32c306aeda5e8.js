8d89366f93bffbceb9ee9fef5e7af3214654e943
Tongjie <tjwang0221@gmail.com>
2019-08-08 01:45:11 -0700

    github page

---
+++ precache-manifest.364a562836a84dbc03f32c306aeda5e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "667ab2f225929756f2180af77850217d",
+    "url": "/fake-code-typer/index.html"
+  },
+  {
+    "revision": "d2fcfe4d7f9a6591335c",
+    "url": "/fake-code-typer/static/css/2.72d72b5e.chunk.css"
+  },
+  {
+    "revision": "eec4490189779e9ae2dc",
+    "url": "/fake-code-typer/static/css/main.53760348.chunk.css"
+  },
+  {
+    "revision": "d2fcfe4d7f9a6591335c",
+    "url": "/fake-code-typer/static/js/2.cf2a4753.chunk.js"
+  },
+  {
+    "revision": "eec4490189779e9ae2dc",
+    "url": "/fake-code-typer/static/js/main.2797b60e.chunk.js"
+  },
+  {
+    "revision": "ba15fad5a8b88b6d9eb8",
+    "url": "/fake-code-typer/static/js/runtime~main.0438f313.js"
+  },
+  {
+    "revision": "454c21c783a2f5b99dfde4ee641bad59",
+    "url": "/fake-code-typer/static/media/info-circle.454c21c7.svg"
+  }
+]);
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import FakeCodeTyper from './FakeCodeTyper';
-
-ReactDOM.render(<FakeCodeTyper />, document.getElementById('root'));
